5b2bf18b241070204b044fe7cf9759fd0f6c9167
Cristian Weiland <cw14@inf.ufpr.br>
2016-06-14 13:03:17 -0300

    Permite selecionar varios tiles e inseri-los ao mesmo tempo
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

--- Cria_Fase_Linux/faz_isom.c
+++ Cria_Fase_Linux/faz_isom.c
@@ -2,8 +2,8 @@
 #include "faz_isom.h"
 
 #define n 10000
-#define xmenu 850
-#define ymenu 450
+#define XMENU 850
+#define YMENU 450
 #define MIN_X_CORTE 0
 #define MAX_X_CORTE 576
 #define MIN_Y_CORTE 32
@@ -77,7 +77,7 @@ int main() {
     ALLEGRO_BITMAP *tileiso = al_load_bitmap("tile_iso.bmp");
     ALLEGRO_BITMAP *tilemenu = al_load_bitmap("menu_iso.bmp");
     ALLEGRO_FONT *font = al_load_font("font.tga", 12, 0);
-    int i, j, largura = 1;
+    int i, j, largura_pincel = 1, largura_sel = 1, altura_sel = 1;
     int xtile[n], ytile[n], xcorte[n], ycorte[n];
     int xcorte_aux = 0, ycorte_aux = 32;
     int aux = -1, pos = 0;
@@ -131,28 +131,32 @@ int main() {
             32 <= icorte <= 992
             */
             if(ev.keyboard.keycode == ALLEGRO_KEY_L) {
-                if(xcorte_aux < MAX_X_CORTE)
+                if(xcorte_aux + 64*(largura_sel-1) < MAX_X_CORTE) {
                     xcorte_aux += 64;
-                redraw = true;
+                    redraw = true;
+                }
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_H) {
-                if(xcorte_aux > MIN_X_CORTE)
+                if(xcorte_aux > MIN_X_CORTE) {
                     xcorte_aux -= 64;
-                redraw = true;
+                    redraw = true;
+                }
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_J) {
-                if(ycorte_aux < MAX_Y_CORTE)
+                if(ycorte_aux + 32*(altura_sel-1) < MAX_Y_CORTE) {
                     ycorte_aux += 32;
-                redraw = true;
+                    redraw = true;
+                }
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_K) {
-                if(ycorte_aux > MIN_Y_CORTE)
+                if(ycorte_aux > MIN_Y_CORTE) {
                     ycorte_aux -= 32;
-                redraw = true;
+                    redraw = true;
+                }
             }
             /* Escolhe tile indo pro extremo cima/esquerda ou extremo esquerda/direita */
             if(ev.keyboard.keycode == ALLEGRO_KEY_O) {
-                xcorte_aux = MAX_X_CORTE;
+                xcorte_aux = MAX_X_CORTE - 64*(largura_sel-1);
                 redraw = true;
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_Y) {
@@ -160,24 +164,58 @@ int main() {
                 redraw = true;
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_U) {
-                ycorte_aux = MAX_Y_CORTE;
+                ycorte_aux = MAX_Y_CORTE - 32*(altura_sel-1);
                 redraw = true;
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_I) {
                 ycorte_aux = MIN_Y_CORTE;
                 redraw = true;
             }
-
-
+            /* Controla o número de tiles inseridos de cada vez. */
             if(ev.keyboard.keycode == ALLEGRO_KEY_Q) {
-                if(largura <= 7)
-                    largura += 2;
-                redraw = true;
+                if(largura_pincel <= 7) {
+                    largura_pincel += 2;
+                    redraw = true;
+                    largura_sel = 1;
+                    altura_sel = 1;
+                }
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_W) {
-                if(largura > 1)
-                    largura -=2;
-                redraw = true;
+                if(largura_pincel > 1) {
+                    largura_pincel -= 2;
+                    redraw = true;
+                    largura_sel = 1;
+                    altura_sel = 1;
+                }
+            }
+            /* Controla o número de tiles selecionados de cada vez. */
+            if(ev.keyboard.keycode == ALLEGRO_KEY_D && xcorte_aux + 64*(largura_sel-1) < MAX_X_CORTE) {
+                if(largura_sel <= 7) {
+                    largura_sel += 1;
+                    redraw = true;
+                    largura_pincel = 1;
+                }
+            }
+            if(ev.keyboard.keycode == ALLEGRO_KEY_F) {
+                if(largura_sel > 1) {
+                    largura_sel -= 1;
+                    redraw = true;
+                    largura_pincel = 1;
+                }
+            }
+            if(ev.keyboard.keycode == ALLEGRO_KEY_E) {
+                if(altura_sel <= 7 && ycorte_aux + 32*(altura_sel-1) < MAX_Y_CORTE) { // Não pode passar do limite pra baixo. Tá errado.
+                    altura_sel += 1;
+                    redraw = true;
+                    largura_pincel = 1;
+                }
+            }
+            if(ev.keyboard.keycode == ALLEGRO_KEY_R) {
+                if(altura_sel > 1) {
+                    altura_sel -= 1;
+                    redraw = true;
+                    largura_pincel = 1;
+                }
             }
             if(ev.keyboard.keycode == ALLEGRO_KEY_A) {
                 arquivo = fopen("lago.txt", "r");
@@ -225,36 +263,30 @@ int main() {
             salvo = false;
         }
 
-        if(mouse_b & 1) { // Clicou com o botão esquerdo. Adiciona um tile. Funciona. Fazer pra + de 1 tile simultaneo.
-            /* Funciona pra 1 tile.
-            xtile[index] = pos_x;
-            ytile[index] = pos_y;
-            xcorte[index] = xcorte_aux;
-            ycorte[index] = ycorte_aux;
-            ++index;
-            */
-            /*
-            352 208
-            384 192
-            416 176
-            +32 -16
-
-            384 224
-            416 208
-            448 192
-            +32 -16
-
-            416 240
-            448 224
-            480 208            
-            */
-            for(i=0; i<largura; ++i) {
-                for(j=0; j<largura; ++j) {
-                    xtile[index] = pos_x+32*(j+i);
-                    ytile[index] = pos_y-16*j+16*   i;
-                    xcorte[index] = xcorte_aux;
-                    ycorte[index] = ycorte_aux;
-                    ++index;
+        if(mouse_b & 1) {
+            /* Se a largura do pincel é maior que 1, é garantido que o tamanho da seleção é 1x1.
+               Portanto, escreve tratando a largura do pincel. */
+            if(largura_pincel > 1)
+                for(i=0; i<largura_pincel; ++i) {
+                    for(j=0; j<largura_pincel; ++j) {
+                        xtile[index] = pos_x+32*(j+i);
+                        ytile[index] = pos_y-16*j+16*i;
+                        xcorte[index] = xcorte_aux;
+                        ycorte[index] = ycorte_aux;
+                        ++index;
+                    }
+                }
+            /* Se a largura for igual a 1, a seleção ou é 1x1 ou é maior. Então trata de um jeito
+               genérico, que funciona pra seleção sendo 1x1 ou qualquer outro tamanho. */
+            else {
+                for(i=0; i<largura_sel; ++i) {
+                    for(j=0; j<altura_sel; ++j) {
+                        xtile[index] = pos_x+64*i;
+                        ytile[index] = pos_y+32*j;
+                        xcorte[index] = xcorte_aux+64*i;
+                        ycorte[index] = ycorte_aux+32*j;
+                        ++index;
+                    }
                 }
             }
         } else if(mouse_b & 2) {
@@ -277,33 +309,40 @@ int main() {
 
             al_clear_to_color(al_map_rgb(0,0,0));
 
+            /* Imprime o mapa. */
             for(j=0; j<index; ++j) {
                 if(xtile[j] >= 0)
                     al_draw_bitmap_region(tileiso, xcorte[j], ycorte[j], 64, 32, xtile[j], ytile[j], 0);
             }
-            /* Funciona pra 1 quadrado, mas não pra multiplos.
-            al_draw_line((float) pos_x   , (float) pos_y+16, (float) pos_x+32, (float) pos_y   , al_map_rgb(0,0,255), 1);
-            al_draw_line((float) pos_x+32, (float) pos_y   , (float) pos_x+64, (float) pos_y+16, al_map_rgb(0,0,255), 1);
-            al_draw_line((float) pos_x+64, (float) pos_y+16, (float) pos_x+32, (float) pos_y+32, al_map_rgb(0,0,255), 1);
-            al_draw_line((float) pos_x+32, (float) pos_y+32, (float) pos_x   , (float) pos_y+16, al_map_rgb(0,0,255), 1);
-            */
-            /* Desenha os quadrados azuis pra indicar onde vou inserir o(s) tile(s). Funciona, aparentemente. */
-            al_draw_line((float) pos_x   , (float) pos_y+16, (float) pos_x+32*largura, (float) pos_y-16*(largura-1)   , al_map_rgb(0,0,255), 1);
-            al_draw_line((float) pos_x+32*(largura), (float) pos_y-16*(largura-1)   , (float) pos_x+64+64*(largura-1), (float) pos_y+16, al_map_rgb(0,0,255), 1);
-            al_draw_line((float) pos_x+64+64*(largura-1), (float) pos_y+16, (float) pos_x+32*largura, (float) pos_y+16+16*(largura), al_map_rgb(0,0,255), 1);
-            al_draw_line((float) pos_x+32*(largura), (float) pos_y+16+16*(largura), (float) pos_x   , (float) pos_y+16, al_map_rgb(0,0,255), 1);
 
-            al_draw_bitmap(tilemenu, xmenu, ymenu, 0);
-            al_draw_rectangle((float) (xmenu - 1), (float) (ymenu + 4), (float) (xmenu + 640/5 + 1), (float) (ymenu + 1024/5 + 5), BRONZE, 1);
-            al_draw_rectangle(((float) xcorte_aux)/5+xmenu, ((float)ycorte_aux)/5+ymenu+8, ((float) xcorte_aux/5)+12+xmenu, ((float) ycorte_aux)/5+ymenu-2, al_map_rgb(0,0,255), 1);
-            al_draw_textf(font, CARMESIM, 20, 20, 0, "Comandos: S (Salvar), Seta <-- e -->, A (Carregar), Q (Expandir), W (Diminuir)");
-            al_draw_textf(font, CARMESIM, 20, 40, 0, "H (Esquerda), J (Baixo), K (Cima), L (Direita)");
-            al_draw_textf(font, CARMESIM, 20, 60, 0, "Y (Esquerda Total), U (Baixo Total), I (Cima Total), O (Direita Total)");
+            /* Desenha os quadrados azuis pra indicar onde vou inserir o(s) tile(s). */
+            al_draw_line((float) pos_x   , (float) pos_y+16, (float) pos_x+32*largura_pincel, (float) pos_y-16*(largura_pincel-1)   , al_map_rgb(0,0,255), 1);
+            al_draw_line((float) pos_x+32*(largura_pincel), (float) pos_y-16*(largura_pincel-1)   , (float) pos_x+64+64*(largura_pincel-1), (float) pos_y+16, al_map_rgb(0,0,255), 1);
+            al_draw_line((float) pos_x+64+64*(largura_pincel-1), (float) pos_y+16, (float) pos_x+32*largura_pincel, (float) pos_y+16+16*(largura_pincel), al_map_rgb(0,0,255), 1);
+            al_draw_line((float) pos_x+32*(largura_pincel), (float) pos_y+16+16*(largura_pincel), (float) pos_x   , (float) pos_y+16, al_map_rgb(0,0,255), 1);
+
+            /* Desenha o menu de tiles. */
+            al_draw_bitmap(tilemenu, XMENU, YMENU, 0);
+
+            /* Desenha o retângulo ao redor do menu de tiles. */
+            al_draw_rectangle((float) (XMENU - 1), (float) (YMENU + 4), (float) (XMENU + 640/5 + 1), (float) (YMENU + 1024/5 + 5), BRONZE, 1);
+
+            /* Desenha o retângulo azul que mostra quais tiles estão selecionados (pra entender essa linha, olhar no explicação 1 no final do código) */
+            al_draw_rectangle(((float) xcorte_aux)/5+XMENU, ((float)ycorte_aux+32*(altura_sel-1))/5+YMENU+8, ((float) (xcorte_aux+64*(largura_sel-1))/5)+12+XMENU, ((float) ycorte_aux)/5+YMENU-2, al_map_rgb(0,0,255), 1);
+
+            /* Imprime comandos básicos. */
+            al_draw_text(font, CARMESIM, 20, 20, 0, "Comandos: S (Salvar), Seta <-- e -->, A (Carregar), Q (Expandir Pincel), W (Diminuir Pincel)");
+            al_draw_text(font, CARMESIM, 20, 40, 0, "E (Expandir Altura Selecao), R (Diminuir Altura Selecao), D (Expandir Largura Selecao)");
+            al_draw_text(font, CARMESIM, 20, 60, 0, "F (Diminuir Largura Selecao), H (Esquerda), J (Baixo), K (Cima), L (Direita)");
+            al_draw_text(font, CARMESIM, 20, 80, 0, "Y (Esquerda Total), U (Baixo Total), I (Cima Total), O (Direita Total)");
+
+            /* Imprime se foram feitas modificações desde a última vez que o arquivo foi salvo. */
             if(salvo)
                 al_draw_text(font, CARMESIM, 20, 100, 0, "Todas as alteracoes foram salvas.");
             else
                 al_draw_text(font, CARMESIM, 20, 100, 0, "Existem alteracoes nao salvas!");
-            //al_draw_textf(font, al_map_rgb(255,0,0), 100, 70, 0, "Xcorte = %d, ycorte = %d", xcorte_aux, ycorte_aux);
+
+            // al_draw_textf(font, al_map_rgb(255,0,0), 100, 70, 0, "Xcorte = %d, ycorte = %d", pos_x, pos_y);
             al_flip_display();
             redraw = false;
         }
@@ -311,3 +350,16 @@ int main() {
     }
     return 0;
 }
+
+/*
+Explicação 1:
+Tenho um retângulo com os vértices a, b, c, d.
+
+a---b
+|   |
+|   |
+c---d
+
+Pra desenhar ele, só preciso de (x,y) de 'a' e (x,y) de 'd', ou (x,y) de 'c' e (x,y) de 'b'.
+Atualmente, eu desenho usando (x,y) de 'c' e 'b'.
+*/
